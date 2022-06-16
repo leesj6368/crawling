f2fbe7cab976fe7e0808d8646327a8061bd5951a
leesj <leesj6368@naver.com>
2022-06-16 16:54:57 +0900

    a

--- 11.py
+++
@@ -1,9 +0,0 @@
-import telegram as tel
-
-bot=tel.Bot(token="5555815826:AAESmlH829POH_XfKdLzL9E8W1rSH1RZM2E")
-
-
-chat_id = bot.getUpdates()[-1].message.chat.id 
-
-bot.sendMessage(chat_id=chat_id,text="test message")
-
